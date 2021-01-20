980abddaa02626faa75c7837d3d21a04b1ae7ccc
David Rueda <davd33@gmail.com>
2021-01-20 14:10:03 +0100

    create a list of products

--- src/kaisse.lisp
+++ src/kaisse.lisp
@@ -43,6 +43,40 @@
                         :padding 3)
     box))
 
+(defun new-model (column-types elements)
+  (let ((model (make-instance 'gtk-list-store
+                              :column-types column-types)))
+
+    (loop :for (name price) :in elements
+          :do (gtk-list-store-set model
+                                  (gtk-list-store-append model)
+                                  name
+                                  price))
+    model))
+
+(defmacro append-new-renderer (view column-name type column-id)
+  `(let* ((renderer (gtk-cell-renderer-text-new))
+          (column (gtk-tree-view-column-new-with-attributes ,column-name
+                                                            renderer
+                                                            ,type
+                                                            ,column-id)))
+     (gtk-tree-view-append-column ,view column)))
+
+(defun new-view (model on-change)
+  (let* ((view (make-instance 'gtk-tree-view
+                              :model model))
+         (select (gtk-tree-view-get-selection view)))
+
+    (append-new-renderer view "Name" "text" 0)
+    (append-new-renderer view "Price" "text" 1)
+
+    (setf (gtk-tree-selection-mode select) :single)
+    (g-signal-connect select "changed"
+                      (lambda (selection)
+                        (funcall on-change view selection)))
+
+    view))
+
 (defun kaisse ()
   "Kaisse, gtk GUI."
   (format t "Starting program.~%")
@@ -50,16 +84,15 @@
     (gtk-progn "Kaisse" root
       (let* (;; welcome message
              (welcome (gtk-label-new "Welcome to Kaisse"))
-             ;; products model
-             (products-model (make-instance 'gtk-list-store
-                                            :column-types '("gchararray" "guint")))
-             ;; product list view
-             (products-view (make-instance 'gtk-tree-view
-                                           :model products-model)))
+             (view (new-view (new-model '("gchararray" "guint") '(("banana" 2)
+                                                                  ("orange" 1)))
+                             (lambda (view selection)
+                               (let* ((model (gtk-tree-view-get-model view))
+                                      (iter (gtk-tree-selection-get-selected selection))
+                                      (name (gtk-tree-model-get-value model iter 0)))
+                                 (format t "You selected ~A.~%" name))))))
 
-        ;; add products into model
-        (gtk-list-store-set products-model (gtk-list-store-append products-model)
-                            "Klaus-Dieter Mustermann" 51)
 
         ;; add to root
-        (gtk-box-pack-start root welcome)))))
+        (gtk-box-pack-end root view)
+        (gtk-box-pack-end root welcome)))))
